065e48d45df77e1a822c5ce86ad63cd103bb96eb
kyoungholee <dlrudgh1303@gmail.com>
2024-01-17 00:01:46 +0900

    style : 돈에 관련된 홈페이지 구현 중

--- src/app/components/home/page.tsx
+++ src/app/components/home/page.tsx
@@ -2,33 +2,116 @@ import React from 'react'
 import SideBar from '../sideBar/page'
 import Image from 'next/image'
 import logo from '/public/moneylogo.png'
+import money from '/public/whatmoney.jpg';
+import Passbook from '/public/Passbook.png';
+import superrich from '/public/superrich.png';
+import realEstate from '/public/realEstate.png';
+import saving01 from '/public/saving01.png';
+import saving02 from '/public/saving02.png';
+import growth from '/public/growth.jpg';
+
+import coin from '/public/coin.png';
+
+
+
 
 export default function HomePage() {
   return (
         <>
         <header className='flex justify-between py-4 bg-white'>
             <div className='flex items-center gap-4'>
-            <Image src={logo}  alt='로고' className='w-20 h-10'/>
-            Money 관리법
+            ALIVE-MONEY
             </div>
             <SideBar/>
         </header> 
         <main>
-            <section className='py-10 mb-4'>돈좀 모으고 싶다하는 사람들만 주목하세요!!</section>
-            <section className='mb-4'>건강한 경제력을 키우는 돈 관리 프로젝트</section>
+            <section className='py-4 text-center bg-gray-300 '>돈좀 모으고 싶다하는 사람들만 주목하세요!!</section>
 
-            <section className='mb-4'>돈이란 무엇인가?</section>
+            {/* <section className='mb-4'>건강한 경제력을 키우는 돈 관리 프로젝트</section> */}
 
-            <section className='mb-4'>가계부, 돈을 관리하는 방법</section>
+            <section className='py-4'>
+                <h1 className='text-xl text-center'>돈이란 무엇인가?</h1>
 
-            <section className='mb-4'>월급으로 1억 모으는 방법</section>
+            <div className='flex gap-4 mt-4 text-lg'>
+            <Image src={money}  alt='돈' className='w-1/5'/>
+            <p>
+                돈은 교환 매체로 사용되는 통화나 가치를 나타내는 일반적인 용어입니다. 다양한 형태의 돈이 존재하지만, 주로 통용되는 것은 국가에서 발행한 화폐입니다.
+                
+                돈은 주로 다음과 같은 기능을 수행합니다:
+                교환 매체: 돈은 상품이나 서비스와 교환할 때 사용되는 매체로 기능합니다. 사람들은 돈을 사용하여 물건을 사거나 서비스를 받습니다.
+                가치의 단위: 돈은 가치의 표시로 사용됩니다. 상품이나 서비스의 가치를 돈의 양으로 나타내어 비교할 수 있습니다.
+                저장 수단: 돈은 가치를 저장할 때 사용됩니다. 사람들은 돈을 저축하거나 투자하여 미래에 사용하기 위해 보관합니다.
+                계산 단위: 돈은 통화의 형태로 사용되어 거래나 계산을 편리하게 만듭니다. 금전화된 가치의 표시로서 쉽게 계산할 수 있습니다.
+                다양한 형태의 돈이 있습니다. 예를 들어, 현금, 은행 소유의 예금, 전자화폐 등이 그 예입니다. 돈은 경제 시스템에서 중요한 역할을 하며, 사회적 교환과 생활의 편의성에 큰 기여를 합니다.
+                </p>
+            </div>
+            
+            </section>
+            
+            <section className='py-4 px-28'>
+                <div className='flex gap-20 mt-4 text-lg'>
+                <Image src={saving02}  alt='가계부 로고' className='w-1/5'/>
+                <div className='flex items-center'>
+                        <div>
+                            <h2 className='mb-8 text-2xl notoSansKr'><strong>돈 저축 * 가계부로 시작합시다.</strong></h2>
+                            <p className='text-xl leading-8'>
+                            가계부 정리 뭐 어디서 부터 시작해야되지..?<br />
+                            돈 모을라면 가계부 적어야 하나?<br />
+                            이러한 분들을 위해 가계부를 쉽게 적어보고, 남들과 함꼐 공유해요!
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            
+            <section className='py-4 px-28'>
+                <div className='flex gap-20 mt-4 text-lg'>
+                    <Image src={growth}  alt='월급 로고' className='w-1/5'/>
+                    <div className='flex items-center'>
+                        <div>
+                            <h2 className='mb-8 text-2xl notoSansKr'><strong>1억을 모을 수 있을까~?</strong></h2>
+                            <p className='text-xl leading-8'>
+                            1억을 모으면 뭘 하지~?<br />
+                            모을 수 있는 방법은?<br />
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
 
-            <section className='mb-4'>주식,코인, 부동산에 대한 정보</section>
+            <section className='py-4 px-28'>
+                <div className='flex gap-20 mt-4 text-lg'>
+                <Image src={Passbook}  alt='통장 로고' className='w-1/5'/>
+                    <div className='flex items-center'>
+                        <div>
+                            <h2 className='mb-8 text-2xl notoSansKr'><strong>적금, 은행에 넣기</strong></h2>
+                            <p className='text-xl leading-8'>
+                                은행 적금과 예금<br />
+                                너무 많은 예/적금이 있는데 뭘 들어야 하지~?<br />
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            
+            <section className='py-4 px-28'>
+                <div className='flex gap-20 mt-4 text-lg'>
+                    <Image src={coin}  alt='재테크 로고' className='w-1/5'/>
+                    <div className='flex items-center'>
+                        <div>
+                            <h2 className='mb-8 text-2xl notoSansKr'><strong>돈만 생각하면 불안한 마음, 재테크 정보</strong></h2>
+                            <p className='text-xl leading-8'>
+                            재테크 공부..<br />
+                            주식, 부동산, 코인 뭐 부터 시작 해야 할까요?<br />
+                            같이 공유하고 공부합시다.<br />
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
 
-            <section className='mb-4'>안전자산(예금, 적금)</section>
 
-
-            <section className='mb-4'>1억을 모으면 그 다음은....</section>
+            <section className='py-4 bg-gray-100'>1억을 모으면 그 다음은....</section>
 
         </main>
         </>
