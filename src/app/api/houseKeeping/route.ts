591f226ea3afbca1cd5e3bfc79c80b74013dafe9
kyoungholee <dlrudgh1303@gmail.com>
2024-02-04 01:43:18 +0900

    feat : 가계부에 데이터 입력시 db에 저장 될 수 있게 구현

--- src/app/api/houseKeeping/route.ts
+++ src/app/api/houseKeeping/route.ts
@@ -1,23 +1,26 @@
 import { Request, Response } from "express";
+import { NextResponse } from "next/server";
 import connectMongoDB from "server/libs/mongodb";
 import recordMoney from "server/models/recordMoney";
 
-export async function POST(req: Request, res: Response) {
+export async function POST(req: any, res: Response) {
   try {
-    const { category, amount, description } = req.body;
+    const { category, amount, description } = await req.json();
     console.log("post로 들어온 데이터", category, amount, description);
 
     // MongoDB 연결
     await connectMongoDB();
 
-
-      // MongoDB에 데이터 저장
-      const newRecord = await recordMoney.create({ category, amount, description });
-
     // MongoDB에 데이터 저장 로직 추가
     // 예시: await SomeMongoDBModel.create({ category, amount, description, name });
+    const newRecord = await recordMoney.create({ category, amount, description });
 
-    res.status(200).json({ success: true, message: "Data successfully saved to MongoDB." });
+    // res.status(200).json({ success: true, message: "Data successfully saved to MongoDB." });
+
+    return NextResponse.json(
+      { message: "가계부 작성~~!!~~ 축하합니다." },
+      { status: 201 }
+    );
   } catch (error) {
     console.error("Error while processing POST request:", error);
     res.status(500).json({ success: false, message: "Internal server error." });
