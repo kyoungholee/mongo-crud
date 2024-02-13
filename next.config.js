571ba97fa0cddece9edd3dc848c866114745b4e8
kyoungholee <dlrudgh1303@gmail.com>
2024-02-14 00:36:51 +0900

    feat : 웹팩 존속성 오류 해결 -->webpack5를 설정해주자

--- next.config.js
+++ next.config.js
@@ -5,6 +5,6 @@ const nextConfig = {
         // your project has ESLint errors.
         ignoreDuringBuilds: true,
       },
+      webpack5: true, webpack: (config, options) => { config.cache = false; return config; },
 }
 
-module.exports = nextConfig
