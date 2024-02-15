5d2afa31bbf5aa14320bf187f6513a9984688452
kyoungholee <dlrudgh1303@gmail.com>
2024-02-15 22:45:06 +0900

    fix : 로그인을 하기 위해 interface type 코드 수정

--- server/interfaces/IUser.ts
+++ server/interfaces/IUser.ts
@@ -1,10 +1,10 @@
-export interface IUser{
-    name: string;
-    email: string;
-    password: string;
-    // avatar: string;
-    // date: Date;
-}
+// export interface IUser{
+//     name: string;
+//     email: string;
+//     password: string;
+//     // avatar: string;
+//     // date: Date;
+// }
 
 export interface IUserInputDTO {
     name: string;
--- server/models/register.ts
+++ server/models/register.ts
@@ -1,19 +1,26 @@
 import mongoose, {Schema} from "mongoose";
-import {IUser} from "../interfaces/IUser";
 
 const RegisterSchema = new Schema({
-   
-    name : {
+    name: {
         type: String,
     },
-    email : {
+    email: {
         type: String,
         unique: true,
+        index: true, // 인덱스 정의 추가
     },
-    password : {
+    password: {
         type: String,
     },
-
 });
 
-export default mongoose.model<IUser & mongoose.Document>("Register", RegisterSchema);
+// IUser 인터페이스를 먼저 정의
+export interface IUser {
+    name: string;
+    email: string;
+    password: string;
+    // avatar: string;
+    // date: Date;
+}
+
+export default mongoose.model<IUser & mongoose.Document>("RegisterMoneyUser", RegisterSchema); // 모델 이름과 컬렉션 이름을 일치시킴
