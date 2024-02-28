c9d0733e2c056dfd3ea8daeef536de5488547ca4
kyoungholee <dlrudgh1303@gmail.com>
2024-02-28 23:08:58 +0900

    hotfix :  userid별로 저장한 값을 구분하기 위해  스키마에 userid 타입을 추가

--- server/interfaces/IRecord.ts
+++ server/interfaces/IRecord.ts
@@ -3,4 +3,6 @@ export interface IRecord {
     category : string;
     amount : string;
     description: string;
+    userid:string;
+
 }
--- server/models/recordMoney.ts
+++ server/models/recordMoney.ts
@@ -1,11 +1,6 @@
 import mongoose, { Schema } from "mongoose";
 import { IRecord } from "../interfaces/IRecord";
 
-// 로그인한 사용자의 ID를 어떻게 구하는지에 따라서 적절히 설정합니다.
-const loggedInUserId = 'zczc14'; // 예시로 사용자 ID를 하드코딩한 것입니다. 실제 사용자 ID를 여기에 적용해야 합니다.
-
-// 사용자 ID를 포함한 모델 이름 생성
-const modelName = `RecordMoneies_${loggedInUserId}`;
 
 const RecordMoneySchema = new Schema({
     category: {
@@ -17,9 +12,12 @@ const RecordMoneySchema = new Schema({
     description: {
         type: String,
     },
+    userid: {
+        type: String,
+    },
 });
 
 // 동적으로 모델 생성
-const RecordModel = mongoose.model<IRecord & mongoose.Document>(modelName, RecordMoneySchema);
+const RecordModel = mongoose.model<IRecord & mongoose.Document>("savemoney", RecordMoneySchema);
 
 export default RecordModel;
