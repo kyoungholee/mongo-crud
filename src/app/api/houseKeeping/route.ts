b48616f78532b83b1afd3a20a4a6fc1ad52a7799
kyoungholee <dlrudgh1303@gmail.com>
2024-03-10 00:32:27 +0900

    feat : 가게부 작성 시 회원을 구분하기 위해 userid를 db에 함께 저장 될 수 있도록 구현

--- src/app/api/houseKeeping/route.ts
+++ src/app/api/houseKeeping/route.ts
@@ -1,28 +1,31 @@
-import { Request, Response } from "express";
-import { NextResponse } from "next/server";
-import connectMongoDB from "server/libs/mongodb";
-import recordMoney from "server/models/recordMoney";
+// route.ts
 
-export async function POST(req: any, res: Response) {
+import { Request, Response } from 'express';
+import { NextResponse } from 'next/server';
+import connectMongoDB from 'server/libs/mongodb';
+import recordMoney from 'server/models/recordMoney';
+import { setCookie, getCookie } from 'cookies-next';
+
+
+export async function POST(req: any, res: any) {
   try {
-    const { category, amount, description } = await req.json();
-    console.log("post로 들어온 데이터", category, amount, description);
+    const { category, amount, description, userid} = await req.json();
+    console.log("post로 들어온 데이터", category, amount, description , userid);
 
     // MongoDB 연결
     await connectMongoDB();
 
-    // MongoDB에 데이터 저장 로직 추가
-    // 예시: await SomeMongoDBModel.create({ category, amount, description, name });
-    const newRecord = await recordMoney.create({ category, amount, description });
 
-    // res.status(200).json({ success: true, message: "Data successfully saved to MongoDB." });
+    // MongoDB에 데이터 저장 로직 추가
+    const newRecord = await recordMoney.create({ category, amount, description, userid});
 
     return NextResponse.json(
-      { message: "가계부 작성~~!!~~ 축하합니다." },
+      { message: "월급추!!~!!~~ 축하합니다." },
       { status: 201 }
     );
-  } catch (error) {
-    console.error("Error while processing POST request:", error);
-    res.status(500).json({ success: false, message: "Internal server error." });
-  }
-}
+      }catch(err) {
+        //   console.error("해당 부분 오류 입니다.");
+        //   return res.status(500).json({ message: "서버 오류 발생" });
+      }
+    }
+
