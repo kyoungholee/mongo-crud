ef441e57ca546305b4cb6c84027a90054ca9d08f
kyoungholee <dlrudgh1303@gmail.com>
2024-03-12 00:56:08 +0900

    feat : 가계부에 데이터를 저장하기 버튼을 눌렀을 때, api get을 이용해 이미 db에 모델을 사용했다는 에러가 있어,  조건을 나눠  코드를 수정

--- server/models/recordMoney.ts
+++ server/models/recordMoney.ts
@@ -1,23 +1,29 @@
-import mongoose, { Schema } from "mongoose";
+import mongoose, { Schema, Model, Document } from "mongoose";
 import { IRecord } from "../interfaces/IRecord";
 
+// 이미 존재하는 모델을 재사용하도록 변경
+let RecordModel: Model<IRecord & Document>;
 
-const RecordMoneySchema = new Schema({
-    category: {
-        type: String,
-    },
-    amount: {
-        type: String,
-    },
-    description: {
-        type: String,
-    },
-    userid: {
-        type: String,
-    },
-});
-
-// 동적으로 모델 생성
-const RecordModel = mongoose.model<IRecord & mongoose.Document>("savemoneydata", RecordMoneySchema);
+// 모델이 이미 존재하는지 확인하고 존재하지 않으면 생성
+if (mongoose.models && mongoose.models.savemoneydata) {
+    RecordModel = mongoose.model<IRecord & Document>("savemoneydata");
+} else {
+    const RecordMoneySchema = new Schema({
+        category: {
+            type: String,
+        },
+        amount: {
+            type: String,
+        },
+        description: {
+            type: String,
+        },
+        userid: {
+            type: String,
+        },
+    });
+    
+    RecordModel = mongoose.model<IRecord & Document>("savemoneydata", RecordMoneySchema);
+}
 
 export default RecordModel;
