402cbced27545cf5f937de125b956ed72379b9ad
kyoungholee <dlrudgh1303@gmail.com>
2024-03-15 00:20:41 +0900

    feat : 내역 카테고리 리스트를 utils로 따로 빼서 관리

---
+++ utils/categorydata.ts
@@ -0,0 +1,22 @@
+export const categoryList = [
+    {
+      id: 0,
+      category: 'expenditure', //지출
+      name: 'expenditure',
+    },
+    {
+      id: 1,
+      category: 'Investment', //투자
+      name: 'Investment',
+    },
+    {
+      id: 2,
+      category: 'savings',// 저축
+      name: 'savings',
+    },
+    {
+      id: 3,
+      category: 'Incoming', //수입
+      name: 'Incoming',
+    },
+  ];
