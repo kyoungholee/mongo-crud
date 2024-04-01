ff1b6dcd70c2b86ef46969bcca4cfc7415693863
kyoungholee <dlrudgh1303@gmail.com>
2024-04-01 23:55:01 +0900

    feat : 날짜에 따른 데이터를 저장할때 월별로 데이터를 불러 올 수 있도록  파라미터 추가

--- src/app/api/getHouseKeeping/[id]/route.ts
+++ src/app/api/getHouseKeeping/[id]/route.ts
@@ -2,17 +2,24 @@ import { NextResponse } from "next/server";
 import connectMongoDB from "server/libs/mongodb";
 import recordMoney from "server/models/recordMoney";
 
-export async function GET(request : Request, { params } : any, res: Response) {
+export async function GET(request: Request, { params }: any, res: Response) {
     try {
-        const {id}  = params;
+        const { id, month } = params; // id와 month 파라미터 받아오기
 
         await connectMongoDB();
-        
-        const getSaveData = await recordMoney.find({userid : id}); // findOne() 대신 find() 사용
+
+        const startDate = new Date(month); // 월의 시작일
+        const endDate = new Date(startDate); // 월의 끝일 계산
+        endDate.setMonth(endDate.getMonth() + 1);
+
+        const getSaveData = await recordMoney.find({
+            userid: id,
+            createDate: { $gte: startDate, $lt: endDate } // 해당 월의 범위 지정
+        });
 
         console.log("찾은 데이터 값은 !!??", getSaveData);
 
-        if(getSaveData && getSaveData.length > 0) { // 데이터가 있는지와 길이를 확인
+        if (getSaveData && getSaveData.length > 0) {
             const responseData = getSaveData.map(data => ({
                 category: data.category,
                 amount: data.amount,
@@ -21,7 +28,7 @@ export async function GET(request : Request, { params } : any, res: Response) {
                 // updateDate: data.updateDate,
             }));
 
-            return NextResponse.json(responseData, { status : 201}); // 배열로 반환
+            return NextResponse.json(responseData, { status: 201 });
         } else {
             return NextResponse.json({ message: "해당 사용자의 데이터를 찾을 수 없습니다." }, { status: 404 });
         }
