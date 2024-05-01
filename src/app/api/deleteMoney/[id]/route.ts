3fe828eda7982ab4fa4673e04a6c546211d1c466
kyoungholee <dlrudgh1303@gmail.com>
2024-05-02 00:11:46 +0900

    feat : 삭제 버튼 클릭시 db에서 데이터 삭제하기 기능 구현완료

--- src/app/api/deleteMoney/[id]/route.ts
+++ src/app/api/deleteMoney/[id]/route.ts
@@ -1,23 +1,30 @@
 import { Request, Response } from 'express';
 import connectMongoDB from 'server/libs/mongodb';
 import recordMoney from 'server/models/recordMoney';
+import { NextResponse } from 'next/server';
+import { message } from 'antd';
 
-export async function Delete(req: Request, res: Response) {
+
+export async function DELETE(req: any, { params }: any, res: any) {
   try {
-    const id = req.params.id; // 삭제할 항목의 ID를 URL 파라미터에서 가져옴
+    const { id } = params; // id와 month 파라미터 받아오기 삭제할 항목의 ID를 URL 파라미터에서 가져옴
 
     console.log("삭제할 id", id);
     // MongoDB 연결
     await connectMongoDB();
 
-    // MongoDB에서 해당 ID에 해당하는 항목 삭제
-    await recordMoney.findByIdAndDelete(id);
+    if(id) {
 
+      await recordMoney.deleteMany({ order: id });
+    }
+
+  
     // 삭제가 성공하면 클라이언트에게 200 상태 코드와 성공 메시지를 응답
-    return res.status(200).json({ message: '삭제 성공' });
-  } catch (error) {
-    console.error('삭제 실패:', error);
-    // 삭제 실패 시 500 상태 코드와 오류 메시지를 응답
-    return res.status(500).json({ message: '서버 오류 발생' });
-  }
-}
+    return NextResponse.json(
+      { message: "삭제가 완료되었습니다." },
+      { status: 201 }
+    );
+      }catch(err) {
+       {message: "서버 api 코드 확인해보세요."}
+      }
+    }
