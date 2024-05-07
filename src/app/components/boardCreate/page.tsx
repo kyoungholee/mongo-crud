8cb87ad9a1e724a9a49b9cf159aa389a9f4f3040
kyoungholee <dlrudgh1303@gmail.com>
2024-05-07 22:41:34 +0900

    style : 게시판 생성하기 UI 구현

--- src/app/components/boardCreate/page.tsx
+++ src/app/components/boardCreate/page.tsx
@@ -1,24 +1,62 @@
-import React from 'react';
-interface Iparams {
-    params : string;
-}
-//이 코드가 먼저 발생합니다. 
-// 이유는 ==> 동적으로 id에 값을 불러와 어떠한 게시판을 클릭해준지 알려준다.
+'use client'
+
+import React, { useState } from 'react';
+import { useRouter } from 'next/router';
+
+const NewPostPage = () => {
+  const [title, setTitle] = useState('');
+  const [writer, setWriter] = useState('');
+  const [content, setContent] = useState('');
+  // const router = useRouter();
+
+   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
 
- const getTopicById = async ()=> {
+    // 게시판 생성 API 호출
+    // const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/login`, {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify({ title, content }),
+    // });
 
-  try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/topics`, {
-      cache: "no-store",
-    });
+    // if (response.ok) {
+    //   // router.push('/posts');
+    // }
+  };
 
-    if (!res.ok) {
-      throw new Error("Failed to fetch topic");
-    }
+  return (
+    <div className="container mx-auto mt-8">
+      <h1 className="mb-4 text-3xl font-bold">게시판 생성하기</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-4">
+          <label htmlFor="title" className="block text-sm font-medium text-gray-700">제목</label>
+          <input type="text" id="title" className="w-full p-2 mt-1 border rounded" value={title} onChange={(e) => setTitle(e.target.value)} required />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="content" className="block text-sm font-medium text-gray-700">내용</label>
+          <textarea id="content" rows={5} className="w-full p-2 mt-1 border rounded" value={content} onChange={(e) => setContent(e.target.value)} required></textarea>
+        </div>
 
-    return res.json();
-  } catch (error) {
-    console.log(error);
-  }
+        <div>
+          <div className="mb-4">
+            <label htmlFor="writer" className="block text-sm font-medium text-gray-700">작성자</label>
+            <input type="text" id="writer" className="p-2 mt-1 border rounded w-30" value={writer} onChange={(e) => setWriter(e.target.value)} required />
+          </div>
+          <div className="mb-4">
+          <label htmlFor="writer" className="block text-sm font-medium text-gray-700">날짜</label>
+          <input type="text" id="writer" className="p-2 mt-1 border rounded w-30" value={new Date().toLocaleDateString()} onChange={(e) => setWriter(e.target.value)} required />
+          </div>
+          <div className="mb-4">
+            <button type="submit" className="px-4 py-2 mr-2 text-white bg-blue-500 rounded hover:bg-blue-600">게시글 생성</button>
+            <button type="button" className="px-4 py-2 text-gray-600 bg-gray-200 rounded hover:bg-gray-300">취소</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 };
+
+export default NewPostPage;
