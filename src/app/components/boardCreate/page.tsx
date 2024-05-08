38c3e015744e31af514f658b0bf37dbeb14e2eb5
kyoungholee <dlrudgh1303@gmail.com>
2024-05-09 01:13:09 +0900

    feat : Post 엔드포인트를 이용해  작성한 게시판 정보 api 요청

--- src/app/components/boardCreate/page.tsx
+++ src/app/components/boardCreate/page.tsx
@@ -1,30 +1,48 @@
 'use client'
 
 import React, { useState } from 'react';
-import { useRouter } from 'next/router';
+import axios from 'axios';
+import { useRouter } from "next/navigation";
 
-const NewPostPage = () => {
+
+const NewPostPage: React.FC = () => {
   const [title, setTitle] = useState('');
-  const [writer, setWriter] = useState('');
   const [content, setContent] = useState('');
-  // const router = useRouter();
+  // const [image, setImage] = useState<File | null>(null);
+  const [writer, setWriter] = useState('');
+  const [day, setDay] = useState('');
+  const router = useRouter();
 
-   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  // const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   if (e.target.files && e.target.files[0]) {
+  //     setImage(e.target.files[0]);
+  //   }
+  // };
+
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    
 
+    try {
+      const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/boards`, {
+        title,
+        content,
+        writer,
+        day: new Date().toLocaleDateString()
+      });
 
-    // 게시판 생성 API 호출
-    // const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/login`, {
-    //   method: 'POST',
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //   },
-    //   body: JSON.stringify({ title, content }),
-    // });
-
-    // if (response.ok) {
-    //   // router.push('/posts');
-    // }
+      if (response.status === 201) {
+      console.log("데이터 전송 성공  ", response.data);
+        router.push('/components/boardTopic'); // 성공 시 게시판 목록 페이지로 이동
+      } else {
+        console.error('Failed to create post:', response.data);
+        // 실패 시 예외 처리
+      }
+    } catch (error) {
+      console.error('Failed to create post:', error);
+      // 실패 시 예외 처리
+    }
   };
 
   return (
@@ -39,20 +57,23 @@ const NewPostPage = () => {
           <label htmlFor="content" className="block text-sm font-medium text-gray-700">내용</label>
           <textarea id="content" rows={5} className="w-full p-2 mt-1 border rounded" value={content} onChange={(e) => setContent(e.target.value)} required></textarea>
         </div>
+        
+        {/* <div className="mb-4">
+          <label htmlFor="image" className="block text-sm font-medium text-gray-700">이미지 업로드</label>
+          <input type="file" id="image" className="mt-1" accept="image/*" onChange={handleImageChange} />
+        </div> */}
 
-        <div>
-          <div className="mb-4">
-            <label htmlFor="writer" className="block text-sm font-medium text-gray-700">작성자</label>
-            <input type="text" id="writer" className="p-2 mt-1 border rounded w-30" value={writer} onChange={(e) => setWriter(e.target.value)} required />
-          </div>
-          <div className="mb-4">
-          <label htmlFor="writer" className="block text-sm font-medium text-gray-700">날짜</label>
-          <input type="text" id="writer" className="p-2 mt-1 border rounded w-30" value={new Date().toLocaleDateString()} onChange={(e) => setWriter(e.target.value)} required />
-          </div>
-          <div className="mb-4">
-            <button type="submit" className="px-4 py-2 mr-2 text-white bg-blue-500 rounded hover:bg-blue-600">게시글 생성</button>
-            <button type="button" className="px-4 py-2 text-gray-600 bg-gray-200 rounded hover:bg-gray-300">취소</button>
-          </div>
+        <div className="mb-4">
+          <label htmlFor="writer" className="block text-sm font-medium text-gray-700">작성자</label>
+          <input type="text" id="writer" className="w-full p-2 mt-1 border rounded" value={writer} onChange={(e) => setWriter(e.target.value)} required />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="day" className="block text-sm font-medium text-gray-700">날짜</label>
+          <input type="text" id="day" className="w-full p-2 mt-1 border rounded" value={new Date().toLocaleDateString()} onChange={(e) => setDay(e.target.value)} required />
+        </div>
+        <div className="mb-4">
+          <button type="submit" className="px-4 py-2 mr-2 text-white bg-blue-500 rounded hover:bg-blue-600">게시글 생성</button>
+          <button type="button" className="px-4 py-2 text-gray-600 bg-gray-200 rounded hover:bg-gray-300">취소</button>
         </div>
       </form>
     </div>
