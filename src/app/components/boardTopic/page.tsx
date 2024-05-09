c763c9f615f42f9d1eec4968f1e08cc6739347b8
kyoungholee <dlrudgh1303@gmail.com>
2024-05-10 00:18:32 +0900

    feat :  next 13 버전 부터 getserversideprops를 지원하지 않고 generateMetatdata 를 이용해 동적 데이터를 구현

--- src/app/components/boardTopic/page.tsx
+++ src/app/components/boardTopic/page.tsx
@@ -1,43 +1,59 @@
-'use client'
-
-import React, { useState } from 'react';
 import Link from 'next/link';
+import axios from 'axios';
 
-const BoardTopicPage = () => {
-  const [posts, setPosts] = useState([
-    { id: 1, title: "첫 번째 게시글 제목", content: "게시글 내용 요약..." },
-    // 추가적인 게시글을 필요한 만큼 추가할 수 있습니다.
-  ]);
 
-  const handleDelete = (id :number) => {
-    const updatedPosts = posts.filter(post => post.id !== id);
-    setPosts(updatedPosts);
+
+
+const BoardTopicPage = ({ boards } : any) => {
+
+  console.log("boards", boards);
+  const handleDelete = (id: number) => {
+    // 삭제 로직 구현
   };
 
   return (
     <div className="container mx-auto mt-8">
-        <div className='flex items-start justify-between'>
-            <h1 className="mb-4 text-3xl font-bold">게시글 목록</h1>
-            <button>게시글 생성</button>
-        </div>
-      <ul className="divide-y divide-gray-200">
-        {posts.map(post => (
-          <li key={post.id} className="py-4">
-            <Link href={`/posts/${post.id}`}>
-            {post.title}
+      <div className='flex items-start justify-between'>
+        <h1 className="mb-4 text-3xl font-bold">게시글 목록</h1>
+        <Link href={'/components/boardCreate'}>
+          게시글 생성
+        </Link>
+      </div>
+      {/* <ul className="divide-y divide-gray-200">
+        {boards.map(board => (
+          <li key={board.id} className="py-4">
+            <Link href={`/boards/${board.id}`}>
+              {board.title}
             </Link>
-            <p className="text-gray-500">{post.content}</p>
+            <p className="text-gray-500">{board.content}</p>
             <div className="mt-2">
-              <button onClick={() => handleDelete(post.id)} className="px-4 py-1 mr-2 text-white bg-red-500 rounded hover:bg-red-600">삭제</button>
-              <Link href={`/posts/${post.id}/edit`}>
-               수정
+              <button onClick={() => handleDelete(board.id)} className="px-4 py-1 mr-2 text-white bg-red-500 rounded hover:bg-red-600">삭제</button>
+              <Link href={`/boards/${board.id}/edit`}>
+                수정
               </Link>
             </div>
           </li>
         ))}
-      </ul>
+      </ul> */}
     </div>
   );
 };
+export const generateMetatdata = async (contex : any) => {
+  try {
+    // 게시판 데이터를 가져오기
+    const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/boards`);
+    const boards = response.data;
+
+    // 가져온 데이터를 props로 전달
+    return { 
+      props: {
+        boards
+      } 
+    };
+  } catch (error) {
+    console.error('Failed to fetch boards:', error);
+    return { props: { boards: [] } };
+  }
+}
 
 export default BoardTopicPage;
