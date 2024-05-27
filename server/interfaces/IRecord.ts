c3db76b88f9b6309871fdabb4ea448980a7ed7f6
kyoungholee <dlrudgh1303@gmail.com>
2024-05-27 21:20:17 +0900

    fix : interface 보단 type으로 수정

--- server/interfaces/IRecord.ts
+++ server/interfaces/IRecord.ts
@@ -1,4 +1,7 @@
-export interface IRecord {
+// interface는 중복선언이 가능하기 때문에  잠재적인 에러를 만들 수 있습니다.
+// type 사용을 추천합니다.
+
+export type IRecord =  {
     // id : number;
     category : string;
     amount : string;
