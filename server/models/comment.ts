3345197ae6627cc510d915230f6070604dd56663
kyoungholee <dlrudgh1303@gmail.com>
2024-05-30 20:38:23 +0900

    feat : 게시판 상세페이지에 id를 구분해  id에 맞는 댓글만 보이게 db 구성

--- server/models/comment.ts
+++ server/models/comment.ts
@@ -1,21 +1,19 @@
-import mongoose, {Schema} from "mongoose";
+import mongoose, { Schema } from 'mongoose';
 
-const commentSchema = new Schema(
-    {
-        content: {
-            type : String,
-        },
-        writer: {
-            type : String,
-        },
-        day: {
-            type: String,
-        }
+interface IComment {
+  content: string;
+  writer: string;
+  day: string;
+  postId: string; // 댓글이 속한 게시글 ID
+}
 
-    }
-);
+const CommentSchema: Schema = new Schema({
+  content: { type: String, required: true },
+  writer: { type: String, required: true },
+  day: { type: String, required: true },
+  postId: { type: String, required: true }, // 댓글이 속한 게시글 ID
+});
 
-
-const Comment = mongoose.models.Comment || mongoose.model("Comment", commentSchema);
+const Comment = mongoose.models.Comment || mongoose.model<IComment>('Comment', CommentSchema);
 
 export default Comment;
--- src/app/api/commentApi/route.tsx
+++ src/app/api/commentApi/route.tsx
@@ -2,29 +2,37 @@ import { NextResponse } from "next/server";
 import connectMongoDB from "server/libs/mongodb";
 import Comment from "server/models/comment";
 
-export async function GET() {
-    try {
-      // MongoDB와 연결하여 모든 토픽을 가져옵니다.
-      await connectMongoDB();
-      const comments = await Comment.find();
-  
-      // 토픽을 JSON 형식으로 반환합니다.
-      return NextResponse.json({ comments });
-    } catch (error) {
-      console.error('Failed to fetch comment get 통신:', error);
-      // 오류가 발생한 경우 빈 배열을 반환합니다.
-      return NextResponse.json({ comments: [] });
-    }
+export async function GET(request: any) {
+  try {
+    // MongoDB와 연결
+    await connectMongoDB();
+
+    // 요청에서 postId 쿼리 파라미터 추출
+    const { searchParams } = new URL(request.url);
+    const postId = searchParams.get('postId');
+
+    console.log("postId=========", postId);
+
+    // 특정 게시글에 대한 댓글만 찾기
+    const comments = await Comment.find({ postId });
+
+    // 댓글을 JSON 형식으로 반환
+    return NextResponse.json({ comments });
+  } catch (error) {
+    console.error('Failed to fetch comment GET request:', error);
+    // 오류가 발생한 경우 빈 배열을 반환
+    return NextResponse.json({ comments: [] });
   }
+}
 
 
 export async function POST(request : any) {
-    const { content, writer, day } = await request.json();
+    const { content, writer, day, postId } = await request.json();
     //  const imageFile = request.files?.image; // 이미지 파일
 
     try {
         await connectMongoDB();
-        await Comment.create({ content, writer, day });
+        await Comment.create({ content, writer, day, postId });
         return NextResponse.json({ message: "댓글 생성합니다.!!!~~!!!" }, { status: 201 });
 
     }catch(err) {
