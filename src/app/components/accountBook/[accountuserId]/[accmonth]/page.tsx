3258875aae9750e749b896f0260c07f5e24b76ac
kyoungholee <dlrudgh1303@gmail.com>
2024-06-13 22:06:13 +0900

    feat : 모의 투자에 대한 화면 설계

--- src/app/components/accountBook/[accountuserId]/[accmonth]/page.tsx
+++ src/app/components/accountBook/[accountuserId]/[accmonth]/page.tsx
@@ -297,7 +297,7 @@ if (!userIdCookie || !userMonthCookie) {
             <Link href={`/components/monthlyPrice/${getCookie('userId')}/${getCookie('month')}`}>
             한달 가계부 기록 확인하기
             </Link>
-            <Link href={`/components/monthlyPrice/${getCookie('userId')}/${getCookie('month')}`}>
+            <Link href={`/components/payments/complete}`}>
             모의투자 해보기
             </Link>
           </div>
